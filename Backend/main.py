f1203b779af7836cac3f848beb80f50f6f2a0ae4
NikhilLLm <nshejul9@gmail.com>
2026-02-03 16:24:48 +0530

    Added the fetch and auth routes

--- Backend/main.py
+++ Backend/main.py
@@ -1,14 +1,29 @@
 from fastapi import FastAPI
 from Backend.routes.search import router as search_router
 from Backend.routes.auth import router as auth_router
+from Backend.routes.datafetch import router as datafetch_router
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
 app = FastAPI()
+# CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # frontend origin
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["Authorization", "Content-Type"],
+)
 
 # Register routes
 app.include_router(search_router)
 app.include_router(auth_router)
+app.include_router(datafetch_router)
 
+
+
+
+# Debug: Print all registered routes
 
 if __name__ == "__main__":
     uvicorn.run(
